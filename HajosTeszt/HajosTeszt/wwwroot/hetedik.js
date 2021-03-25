629fd9d3a7fda9669a786c77d8eaa65b5604fae4
Virág Füredi <virag.furedi@gmail.com>
2021-03-25 13:00:48 +0100

    gyakonkesz

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,24 @@
+﻿window.onload = () => {
+    letöltésBefejeződött();
+}
+    var kérdések;
+
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+    
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        console.log(d)
+        kérdések = d;
+        kérdésMegjelenítés(0);
+    }
+    function kérdésMegjelenítés(k) {
+        let ide_kerdes = document.getElementById("kérdés_szöveg.txt")
+        kérdések.innerText = kérdések[k].questionText;
+        for (var i = 1; i <= 3; i++) {
+            document.getElementById(kérdések);
+        }
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[k].image;
+    }
